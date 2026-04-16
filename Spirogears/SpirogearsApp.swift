015610ca1bfedcabdc725732df9d05a1bbf76767
SigridM <sigridm@selfhelpsoftware.com>
2026-04-16 05:32:43 -0400

    Initial Commit

---
+++ Spirogears/SpirogearsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirogearsApp.swift
+//  Spirogears
+//
+//  Created by Sigrid Mortensen on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SpirogearsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
